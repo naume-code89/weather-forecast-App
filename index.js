a3fbcd6183c13217d5ad4d7539cb779f7590ff1c
naume-code89 <naumly89@gmail.com>
2025-08-11 20:17:13 +0200

    made some changes to the files

--- index.js
+++ index.js
@@ -1,10 +1,54 @@
 function refreshWeather(response) {
     let temperatureElement = document.querySelector("#temperature");
     let temperature = response.data.temperature.current;
-
+     console.log(response.data.condition.temperature);
      temperatureElement.innerHTML = Math.round(temperature);
+   
+    let descriptionElement = document.querySelector("#description");
+descriptionElement.innerHTML = response.data.condition.description;
+console.log(response.data.condition.description);
+
+let humidityElement = document.querySelector("#humidity");
+humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+console.log(response.data.temperature.humidity);
+
+let windSpeedElement = document.querySelector("#wind-speed");  
+windSpeedElement.innerHTML = `${response.data.wind.speed}Km/h`;
+console.log(response.data.wind.speed);
+
+let timeElement = document.querySelector("#time");
+let date = new Date(response.data.time * 1000);
+timeElement.innerHTML = formatDate(date);
+
+let iconElement = document.querySelector("#icon");
+iconElement.innerHTML = `<img src="${response.data.condition.icon_url}" class="emoji" />`;
+
 
 }
+
+function formatDate(date) {
+ 
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",]
+      
+ let day = days[date.getDay()];
+if(minutes <10) {
+  minutes = `0$(minutes)`;
+}
+
+ return `${day} ${hours}: ${minutes}`;
+
+ 
+  
+};
 
 
 function searchCity(city) {
